f5c140674c80c5fae1e18aaf95b3f5f4f091517d
ivankitanov <47246947+ivankitanov@users.noreply.github.com>
2019-02-27 22:35:17 +0200

    Blueprints2

--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 app == Blueprint('app', __name__,  template_folder='templates')
 
+students = ['Jack','Geralt','Steven','Pesho','Tony']
+courses = ['Python', 'Maths' , 'English']
+events = ['Board meeting', 'Voleyball tournament', 'Football tournament']
+
 
 @app.route('/')
 def home():
